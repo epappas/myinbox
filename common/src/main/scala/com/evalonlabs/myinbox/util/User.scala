96284926bd355553733f4453f6978a5cd67fe5f7
Evangelos Pappas <papas.evagelos@gmail.com>
2014-04-25 14:38:41 +0100

    implement user util

---
+++ common/src/main/scala/com/evalonlabs/myinbox/util/User.scala
@@ -0,0 +1,34 @@
+package com.evalonlabs.myinbox.util
+
+
+object User {
+
+  def getEncryptionKey(uKey: String): String = {
+    // TODO get user's key from storage and return, block while waiting
+  }
+
+  def getUKey(mailAddr: String): String = {
+    Crypto.md5(mailAddr)
+  }
+
+  def addressFromAlias(alias: String): String = {
+    // TODO check alias from the store
+    alias
+  }
+
+  def checkCredentials(uname: String, pass: String, loop: Int): Boolean = {
+    // TODO fetch user by this uname
+    // TODO IF exists MD5 the pass and check it
+    // TODO return true if all steps succeed
+
+    val modPass = (0 to loop).foldLeft[String](pass: String)((pass: String, index: Int) => {
+      index % 2 match {
+        case 0 => Crypto.md5(pass)
+        case _ => Crypto.sha1(pass)
+      }
+    })
+
+    // TODO return modPass == realPass
+    true
+  }
+}
--- smtp/src/main/scala/com/evalonlabs/myinbox/actor/RecipientCheckActor.scala
+++ smtp/src/main/scala/com/evalonlabs/myinbox/actor/RecipientCheckActor.scala
@@ -3,6 +3,7 @@ package com.evalonlabs.myinbox.actor
 import akka.actor.Actor
 import com.typesafe.scalalogging.slf4j.Logging
 import com.evalonlabs.myinbox.model.AliasAddr
+import com.evalonlabs.myinbox.util.User
 import java.net.InetAddress
 
 class RecipientCheckActor extends Actor with Logging {
@@ -13,7 +14,7 @@ class RecipientCheckActor extends Actor with Logging {
 			//TODO check address
       // TODO if not exists, check alias
       // TODO reject in non existence
-      sender ! AliasAddr(addr)
+      sender ! AliasAddr(User.addressFromAlias(addr))
 		}
 
 		case x => logger error "Unknown message: " + x.toString
