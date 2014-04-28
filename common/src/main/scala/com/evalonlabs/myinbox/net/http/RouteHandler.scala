10900b43f3de2f568790ad6a24f1a520b89afaba
Evangelos Pappas <papas.evagelos@gmail.com>
2014-04-28 08:22:05 +0100

    RouteHandler uses util.Map

--- common/src/main/scala/com/evalonlabs/myinbox/net/http/RouteHandler.scala
+++
@@ -1,7 +0,0 @@
-package com.evalonlabs.myinbox.net.http
-
-import io.netty.channel.ChannelHandlerContext
-
-trait RouteHandler {
-  def handle(ctx: ChannelHandlerContext, path: String, params: Map)
-}
