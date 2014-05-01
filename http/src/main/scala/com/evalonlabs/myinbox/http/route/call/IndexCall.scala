6021b4720bafbcc5d84e50f3de3d5ce4c9a61cc2
Evangelos Pappas <papas.evagelos@gmail.com>
2014-05-01 15:25:08 +0100

    Http index call STUB

---
+++ http/src/main/scala/com/evalonlabs/myinbox/http/route/call/IndexCall.scala
@@ -0,0 +1,19 @@
+package com.evalonlabs.myinbox.http.route.call
+
+import com.evalonlabs.myinbox.net.http.{ContextSystem, RouteHandler, HttpResponse => MyHttpResponse}
+import io.netty.channel.ChannelHandlerContext
+import java.util
+import com.sun.deploy.net.HttpResponse
+import io.netty.handler.codec.http.HttpRequest
+
+class IndexCallHandler(system: ContextSystem) extends RouteHandler {
+
+  override def handle(ctx: ChannelHandlerContext, path: String, params: util.Map[String, Object], request: HttpRequest) {
+    MyHttpResponse.sendOk(ctx, "{\"status\": 200, \"message\": \"OK\"}")
+  }
+}
+
+object IndexCall {
+  def apply(system: ContextSystem): RouteHandler =
+    new IndexCallHandler(system)
+}
