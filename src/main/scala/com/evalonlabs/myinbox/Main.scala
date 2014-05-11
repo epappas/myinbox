a7f8922d5daab296b0c4364391f7a3c14a258636
Evangelos Pappas <papas.evagelos@gmail.com>
2014-05-11 16:59:15 +0100

    no need of Main.scala

--- src/main/scala/com/evalonlabs/myinbox/Main.scala
+++
@@ -1,9 +0,0 @@
-package com.evalonlabs.myinbox
-
-import com.evalonlabs.store.Redis
-
-object Main extends App with Redis {
-  redis.set("helloWorldKey", "Hello World!")
-
-  println(redis.get("helloWorldKey"))
-}
