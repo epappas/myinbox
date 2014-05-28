90fa370dceadc59c3a709259f6e9760e33f46a1c
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-05-28 22:02:25 +0100

    Reject Model is enriched

--- common/src/main/scala/com/evalonlabs/myinbox/model/Reject.scala
+++ common/src/main/scala/com/evalonlabs/myinbox/model/Reject.scala
@@ -1,3 +1,11 @@
 package com.evalonlabs.myinbox.model
 
-case class Reject(reason: String)
+import org.subethamail.smtp.MessageContext
+import java.util.{HashMap => JHashMap}
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.CountDownLatch
+
+case class Reject(reason: String, ctx: MessageContext,
+                  from: String,
+                  state: JHashMap[String, AtomicReference],
+                  lock: CountDownLatch)
