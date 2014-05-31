bacc7c6692a97d548f4abf32c036619ea1aec511
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-05-31 12:39:07 +0100

    Update SenderCheckActor

--- smtp/src/main/scala/com/evalonlabs/myinbox/actor/SenderCheckActor.scala
+++ smtp/src/main/scala/com/evalonlabs/myinbox/actor/SenderCheckActor.scala
@@ -1,30 +1,32 @@
 package com.evalonlabs.myinbox.actor
 
-import akka.actor.Actor
-import com.evalonlabs.myinbox.model.{Message, GoNext}
+import akka.actor.{ActorRef, Actor}
 import com.typesafe.scalalogging.slf4j.Logging
-import java.net.InetAddress
+import java.util.{HashMap => JHashMap}
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.CountDownLatch
+import org.subethamail.smtp.MessageContext
+import com.evalonlabs.myinbox.model.FromReq
+import com.evalonlabs.myinbox.model.FromOk
 
 class SenderCheckActor extends Actor with Logging {
 
-	def receive = {
-
-		case (inet: InetAddress, helo: String, from: String) => {
-			// TODO Flood control check
+  def receive = {
+    case (receiver: ActorRef, FromReq(ctx: MessageContext, from: String),
+    state: JHashMap[String, AtomicReference[String]], lock: CountDownLatch) =>
+      // val mctx = MessageCtxDetails(ctx) // <-- holds all the info that is needed
+      // TODO Flood control check
       // TODO IPFloodFilter.check(inet)
       // TODO DNSBLFilter.check(inet)
       // TODO SPFFilter.check(inet, helo, from)
       // TODO SenderbaseFilter.check(inet)
       // TODO GreylistFilter.check(inet, helo)
 
-      sender ! Message(inet, from)
-		}
-
-		case x => {
-			logger.error("Unknown message: " + x.toString)
-			sender ! GoNext()
-		}
-
-	}
+      receiver !(FromOk(ctx, from), state, lock)
+    // receiver ! (Greylist(reason, ctx, from), state, lock)
+    // receiver ! (Reject(reason, ctx, from), state, lock)
+    case _: Any =>
+      logger.error("Unknown message: " + _.toString)
+  }
 
 }
