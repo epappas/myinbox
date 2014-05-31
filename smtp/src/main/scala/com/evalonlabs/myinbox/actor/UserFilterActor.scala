c31a80ed8a293c48e1ae3f4a37551ba159618326
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-06-01 00:54:26 +0100

    UserFilterActor is updated to use the new models

--- smtp/src/main/scala/com/evalonlabs/myinbox/actor/UserFilterActor.scala
+++ smtp/src/main/scala/com/evalonlabs/myinbox/actor/UserFilterActor.scala
@@ -1,21 +1,25 @@
 package com.evalonlabs.myinbox.actor
 
-import akka.actor.Actor
+import akka.actor.{ActorRef, Actor}
 import javax.mail.internet.MimeMessage
 import com.typesafe.scalalogging.slf4j.Logging
 import scala.language.postfixOps
-import com.evalonlabs.myinbox.model.Message
+import com.evalonlabs.myinbox.model.{UsrFilterOk, UsrFilterReq, RecipientReq, Message}
+import org.subethamail.smtp.MessageContext
+import java.util.{HashMap => JHashMap}
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.CountDownLatch
 
 class UserFilterActor extends Actor with Logging {
 
-	def receive = {
-
-		case message: Message[Any] =>
+  def receive = {
+    case (receiver: ActorRef, UsrFilterReq(ctx: MessageContext, msg: Message[MimeMessage]),
+    state: JHashMap[String, AtomicReference[String]]) =>
       // TODO check this message with User's preferences
-      sender ! message
+      receiver ! (UsrFilterOk(ctx, msg), state)
 
     case msg => logger.error("Unknown message: " + msg.toString)
 
-	}
+  }
 
 }
