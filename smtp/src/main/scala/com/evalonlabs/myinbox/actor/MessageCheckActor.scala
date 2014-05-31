75b1ab217307a21f88955b2700584290ab9cb9ec
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-06-01 00:52:19 +0100

    MessageCheckActor is updated to use the new models

--- smtp/src/main/scala/com/evalonlabs/myinbox/actor/MessageCheckActor.scala
+++ smtp/src/main/scala/com/evalonlabs/myinbox/actor/MessageCheckActor.scala
@@ -1,27 +1,31 @@
 package com.evalonlabs.myinbox.actor
 
-import akka.actor.Actor
+import akka.actor.{ActorRef, Actor}
 import javax.mail.internet.MimeMessage
 import com.evalonlabs.myinbox.util.SafeConfig
-import com.evalonlabs.myinbox.model.{Message, GoNext, Reject}
+import com.evalonlabs.myinbox.model._
 import com.typesafe.scalalogging.slf4j.Logging
+import org.subethamail.smtp.MessageContext
+import java.util.{HashMap => JHashMap}
+import java.util.concurrent.atomic.AtomicReference
+import com.evalonlabs.myinbox.model.Message
 
 class MessageCheckActor extends Actor with Logging {
 
-	def receive = {
+  def receive = {
 
-		case message: Message[MimeMessage] => {
+    case (receiver: ActorRef, MsgCheckReq(ctx: MessageContext, message: Message[MimeMessage]),
+    state: JHashMap[String, AtomicReference[String]]) =>
 
-			message.body.getSize < SafeConfig.getBytes("myinbox.data.max-message-size").getOrElse(100000L).toInt match {
-				case false => sender ! Reject("Ivnvalid")
-				case true =>
+      message.body.getSize < SafeConfig.getBytes("myinbox.data.max-message-size").getOrElse(100000L).toInt match {
+        case false => receiver ! Reject("Ivnvalid", ctx, state.get("sender").get())
+        case true =>
           // TODO Flood control check
-          sender ! message
+          receiver ! (MsgCheckOk(ctx, message), state)
       }
-		}
 
-		case msg => logger.error("Unknown message: " + msg.toString)
+    case msg => logger.error("Unknown message: " + msg.toString)
 
-	}
+  }
 
 }
