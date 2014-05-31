f8631afa3b7e05576f9ca79bdad5b28a0472f757
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-06-01 00:52:56 +0100

    PersistMsgActor is updated to use the new models

--- smtp/src/main/scala/com/evalonlabs/myinbox/actor/PersistMsgActor.scala
+++ smtp/src/main/scala/com/evalonlabs/myinbox/actor/PersistMsgActor.scala
@@ -1,19 +1,22 @@
 package com.evalonlabs.myinbox.actor
 
-import akka.actor.Actor
+import akka.actor.{ActorRef, Actor}
 import javax.mail.internet.MimeMessage
 import com.typesafe.scalalogging.slf4j.Logging
-import java.util.Date
+import java.util.{HashMap => JHashMap, Date}
 import com.evalonlabs.myinbox.util._
 import scala.language.postfixOps
 import com.evalonlabs.myinbox.b2b.Sender
-import com.evalonlabs.myinbox.model.Message
+import com.evalonlabs.myinbox.model.{PersistMsgReq, UsrFilterReq, Message}
+import org.subethamail.smtp.MessageContext
+import java.util.concurrent.atomic.AtomicReference
 
 class PersistMsgActor extends Actor with Logging {
 
   def receive = {
 
-    case message: Message[MimeMessage] => {
+    case (receiver: ActorRef, PersistMsgReq(ctx: MessageContext, message: Message[MimeMessage]),
+    state: JHashMap[String, AtomicReference[String]]) =>
       val cleanMessage = Mail.filterMultiparts(message.body)
       val sentDate = Option(message.body.getSentDate).getOrElse(new Date())
       val messageID = UUID.get()
@@ -28,7 +31,7 @@ class PersistMsgActor extends Actor with Logging {
         Inbox.index(messageID, message.from, message.to, message.subject, new String(messageBody), sentDate)
         Sender.index(message.from, message.inet)
 
-        SmtpActorSystem.userPrefsActor ! (messageID, message)
+        SmtpActorSystem.userPrefsActor !(messageID, message)
 
         // TODO - notify next services
       }
@@ -37,7 +40,6 @@ class PersistMsgActor extends Actor with Logging {
           logger.error("Exception saving message at " + new Date().getTime)
         // TODO Dead letter channel
       }
-    }
 
     case x => logger.error("Unknown message: " + x.toString)
   }
