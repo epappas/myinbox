9d5164f5bd8d71dc6c2a289eafffe93d4ffa421a
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-06-14 17:19:22 +0100

    Make use of supervisor

--- datasmart/src/rest_store_app.erl
+++ datasmart/src/rest_store_app.erl
@@ -16,23 +16,7 @@
 
 %% API.
 
-start(_Type, _Args) ->
-	Dispatch = compile(),
-  {ok, _} = cowboy:start_http(http, 100, [{port, 8080}], [
-    {env, [{dispatch, Dispatch}]}
-  ]),
-	rest_store_sup:start_link().
+start(_Type, _Args) -> rest_store_sup:start_link().
 
 stop(_State) ->
 	ok.
-
-%%% Routing
-compile() ->
-  List = [index_handler],
-  cowboy_router:compile([
-    %% {URIHost, list({URIPath, Handler, Opts})}
-    {'_', [route(Name) || Name <- List]}
-  ]).
-
-route(index_handler) ->
-  {"/index", index_handler, []}.
--- datasmart/src/rest_store_sup.erl
+++ datasmart/src/rest_store_sup.erl
@@ -11,19 +11,45 @@
 -behaviour(supervisor).
 
 %% API.
--export([start_link/0]).
+-export([start_listeners/0, start_link/0]).
 
 %% supervisor.
 -export([init/1]).
 
 %% API.
 
--spec start_link() -> {ok, pid()}.
+start_listeners() ->
+  Port = 8080,
+  ListenerCount = 20,
+
+  Dispatch = compile(),
+  {ok, _} = cowboy:start_http(http, ListenerCount, [{port, Port}], [
+    {env, [{dispatch, Dispatch}]},
+    {timeout, 12000}
+  ]).
+
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%%% Routing
+compile() ->
+  List = [index_handler],
+  cowboy_router:compile([
+    %% {URIHost, list({URIPath, Handler, Opts})}
+    {'_', [route(Name) || Name <- List]}
+  ]).
+
+route(index_handler) ->
+  {"/index", index_handler, []}.
+
 
 %% supervisor.
 
 init([]) ->
-	Procs = [],
-	{ok, {{one_for_one, 10, 10}, Procs}}.
+%%   pg2:create(datastore_rest_listeners),
+  {ok, {{one_for_one, 10, 10}, [
+    {rest_store,
+      {rest_store_sup, start_listeners, []},
+      permanent, 1000, worker,
+      [rest_store_sup]}
+  ]}}.
