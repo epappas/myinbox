a9d5b39c4a1905f239576aeef6703825d5d3ee37
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-06-20 23:29:48 +0100

    Inbox index, stores a short copy of the message

--- common/src/main/scala/com/evalonlabs/myinbox/util/Inbox.scala
+++ common/src/main/scala/com/evalonlabs/myinbox/util/Inbox.scala
@@ -3,17 +3,22 @@ package com.evalonlabs.myinbox.util
 import com.evalonlabs.store.Redis
 import java.util.Date
 
+import scala.util.parsing.json.JSONObject
+
 object Inbox extends Object with Redis {
 
   def add(messageID: String, sender: String, recipient: String, subject: String, gzipMsg: String, date: Date) {
     redis.hset(inboxHSKey(recipient), messageID, gzipMsg)
-    redis.sadd(inboxSSKey(recipient), date.getTime, messageID)
     redis.hset(inboxSendersHSKey(recipient), messageID, sender)
     redis.hset(inboxSubjectHSKey(recipient), messageID, subject)
   }
 
   def index(messageID: String, sender: String, recipient: String, subject: String, msg: String, date: Date) {
-    // TODO
+    redis.sadd(inboxSSKey(recipient), date.getTime, JSONObject(Map(
+      "messageID" -> messageID,
+      "subject" -> subject,
+      "copydrip" -> msg.substring(0, 20)
+    )))
   }
 
   def categorize(uKey: String, messageId: String, category: String) {
