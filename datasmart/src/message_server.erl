d47d82f6b0c06d8c09f26afbba8f6a91a5daf335
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-07-13 12:11:16 +0100

    Datasmart: Alterations in message_server, Store & msginfo

--- datasmart/src/message_server.erl
+++ datasmart/src/message_server.erl
@@ -1,10 +1,9 @@
 %%%-------------------------------------------------------------------
 %%% @author evangelosp
-%%% @copyright (C) 2014, <COMPANY>
+%%% @copyright (C) 2014, evalonlabs
 %%% @doc
 %%%
 %%% @end
-%%% Created : 25. Jun 2014 22:10
 %%%-------------------------------------------------------------------
 -module(message_server).
 -author("evangelosp").
@@ -13,7 +12,9 @@
 
 %% API
 -export([start_link/0,
-  store/3
+  store/3,
+  index/3,
+  msginfo/2
 ]).
 
 %% gen_server callbacks
@@ -39,6 +40,14 @@ store(MessageID, Ukey, Message) ->
   gen_server:cast(?MODULE, {store, MessageID, Ukey, Message}),
   {ok, MessageID}.
 
+index(MessageID, Ukey, Message) ->
+  gen_server:call(?MODULE, {index, MessageID, Ukey, Message}),
+  {ok, MessageID}.
+
+msginfo(MessageID, Ukey) ->
+  gen_server:call(?MODULE, {msginfo, MessageID, Ukey}),
+  {ok, MessageID}.
+
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
@@ -46,17 +55,21 @@ store(MessageID, Ukey, Message) ->
 init([]) ->
   {ok, #state{}}.
 
-handle_call({compress, Message}, _From, State) -> {reply, {ok, doCompress(Message)}, State};
+handle_call({index, MessageID, Ukey, Message}, _From, State) ->
+  {reply, {ok, doIndex(MessageID, Ukey, Message)}, State};
 
-handle_call({index, Message}, _From, State) -> {reply, {ok, doCompress(Message)}, State};
+handle_call({msginfo, MessageID, Ukey}, _From, State) ->
+  {reply, {ok, fetchMsginfo(MessageID, Ukey)}, State};
 
-handle_call({store, Message}, _From, State) -> {reply, {ok, doCompress(Message)}, State};
+handle_call({listbydate, Ukey, FromTS, ToTS}, _From, State) ->
+  {reply, {ok, fetchMsgList(listbydate, Ukey, FromTS, ToTS)}, State};
 
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
 
 handle_cast({store, MessageID, Ukey, Message}, State) ->
-  Now = os:timestamp(),
+  {MegaSecs, Secs, MicroSecs} = os:timestamp(),
+  Now = (MegaSecs * 1000000000000) + (Secs * 1000000) + MicroSecs,
   IVec = uuid:to_string(uuid:uuid3(uuid:uuid4(), uuid:to_string(uuid:uuid1()))),
   Body = proplists:get_value(body, Message),
   Copydrip = string:substr(Body, 1, 20),
@@ -66,7 +79,7 @@ handle_cast({store, MessageID, Ukey, Message}, State) ->
   MetaJson = jiffy:encode(Meta),
 
   {ok, Encrypted} = user_server:encrypt(Ukey, IVec, Body),
-  In64 = base64:encode(Encrypted),
+  In64 = base64:encode_to_string(Encrypted),
   Compressed = doCompress(In64),
 
   qredis:q(["HSET", lists:concat(["datasmart:users:", Ukey, ":inbox:messages"]), MessageID, Compressed]),
@@ -96,3 +109,45 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 
 doCompress(Text) -> zlib:gzip(Text).
+
+doUncompress(Data) -> zlib:gunzip(Data).
+
+doIndex(MessageID, Ukey, Message) ->
+%%   TODO Index meesage to ES or Other search engine
+%%   TODO Break message to Ngram of 1-5
+  {[]}.
+
+fetchMsgList(listbydate, Ukey, FromTS, ToTS) ->
+  {ok, Result} = qredis:q(["ZREVRANGEBYSCORE", lists:concat(["datasmart:users:", Ukey, ":inbox:list"]), FromTS, ToTS]),
+  KeyValList = ds_util:list_to_keyval_rev(Result),
+  FoldedList = lists:foldl(fun({Timestamp, MessageID}, AccIn) ->
+    {ok, Subject} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:subject"]), MessageID]),
+    {ok, From} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:sender"]), MessageID]),
+    {ok, Copydrip} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:copydrip"]), MessageID]),
+    AccIn ++ [
+      {datetime, Timestamp},
+      {messageID, MessageID},
+      {subject, Subject},
+      {from, From},
+      {copydrip, Copydrip}
+    ]
+  end, [], KeyValList),
+  {FoldedList}.
+
+fetchMsginfo(MessageID, Ukey) ->
+  {ok, Compressed} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:messages"]), MessageID]),
+  {ok, MetaJson} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:meta"]), MessageID]),
+  {ok, Subject} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:subject"]), MessageID]),
+  {ok, From} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:sender"]), MessageID]),
+  {ok, IVec} = qredis:q(["HGET", lists:concat(["datasmart:users:", Ukey, ":inbox:ivec"]), MessageID]),
+
+  Decompressed = doUncompress(Compressed),
+  Encrypted = base64:decode_to_string(Decompressed),
+  {ok, Dencrypted} = user_server:dencrypt(Ukey, IVec, Encrypted),
+
+  {[
+    {from, From},
+    {subject, Subject},
+    {body, Dencrypted},
+    {meta, jiffy:decode(MetaJson)}
+  ]}.
