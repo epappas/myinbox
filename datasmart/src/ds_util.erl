294dda6f6b874fc1e7a9c5ad0dd43a7b83db2acd
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-07-17 07:47:43 +0100

    Datasmart: timestamp in ds_util

--- datasmart/src/ds_util.erl
+++ datasmart/src/ds_util.erl
@@ -11,8 +11,22 @@
 %% API
 -export([
   list_to_keyval/1,
-  list_to_keyval_rev/1
+  list_to_keyval_rev/1,
+  timestamp/0,
+  timestamp/1,
+  now/0,
+  now/1
 ]).
+
+timestamp() -> timestamp(os:timestamp()).
+
+timestamp({MegaSecs, Secs, MicroSecs}) ->
+  (MegaSecs * 1000000000000) + (Secs * 1000000) + MicroSecs.
+
+now() -> now(os:timestamp()).
+
+now({MegaSecs, Secs, _MicroSecs}) ->
+  (MegaSecs * 1000000) + Secs.
 
 list_to_keyval([]) -> [];
 
