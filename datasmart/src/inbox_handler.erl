8cf21b399c255dacf58751eb7c8ec4ba554c0cce
Evangelos Pappas <evangelos.pappas@matchesfashion.com>
2014-07-19 13:12:17 +0100

    Datasmart: Inbox List

--- datasmart/src/inbox_handler.erl
+++ datasmart/src/inbox_handler.erl
@@ -21,7 +21,8 @@ handle(Req, State) ->
   {ok, Req3, State}.
 
 maybe_response(<<"GET">>, Req) ->
-  {OUkey, Req2} = cowboy_req:qs_val(<<"user">>, Req),
+  {OUkeyBin, Req2} = cowboy_req:qs_val(<<"user">>, Req),
+  OUkey = binary:bin_to_list(OUkeyBin),
   case handle_query(OUkey, Req2) of
     {list, MList, _} ->
       echo(200, jiffy:encode({[
@@ -45,9 +46,11 @@ maybe_response(_, Req) ->
 
 handle_query(OUkey, Req) ->
   {ok, Ukey} = user_server:match_ouKey(OUkey),
-  {ok, InboxList} = {ok, []},
-%%   TODO Fetch List of Inbox Messages
-  {list, [ensureInboxItemJson(Item) || Item <- InboxList], Req}.
+  case message_server:msgList(date, Ukey, ds_util:timestamp(), ds_util:timestamp() - (3600 * 24 * 1000)) of
+    {ok, {ok,{[]}}} -> {list, [], Req};
+    {ok, {ok,{InboxList}}} ->
+      {list, [ensureInboxItemJson(Item) || Item <- InboxList], Req}
+  end.
 
 echo(Status, Echo, Req) ->
   cowboy_req:reply(Status, [
